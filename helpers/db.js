5cb9081c5b44c3fc77e0920742db71b6833c8acd
heriistantoo <heriistantoo@gmail.com>
2022-01-24 21:25:56 +0700

    update library whatsapp error scan

--- helpers/db.js
+++ helpers/db.js
@@ -2,9 +2,15 @@ const { query } = require('express-validator');
 const { Client } = require('pg');
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,  //Online
+    //Online
+    connectionString: process.env.DATABASE_URL,  
+    
+    //db-main localhost
     //connectionString: 'postgres://rqdhjiijzhxdnb:a17bcb235f2309297736154f05690b3d5715ef0987713084418b06e1fd4d62e9@ec2-18-233-83-165.compute-1.amazonaws.com:5432/dd9i2ie6fgp9cq',
     
+    //db-backup localhost
+    //connectionString: 'postgres://gqhxgnvfytguiu:eb2007fc968564e496102eecd4393a7bbfb557b2ac7f15e433728275a3b16711@ec2-54-147-93-73.compute-1.amazonaws.com:5432/d2907res4rg2fd',
+
     ssl: {
         rejectUnauthorized: false
     }
